7cada99d7b1e14d5696a921dfa3abd03f458925e
Your Name <your_email@example.com>
2023-03-08 12:34:08 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/homepage.dart';
 
-
-
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key }) : super(key : null);
+  const MyApp({Key}) : super(key: null);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child:Container(
-            child: Text("welcome to  flutter"),
-          ),      
-        ),
-      ), 
+      home: Homepage( ),
     );
   }
 }
-
-
-
-
